28a1153660885c8503181638b400008966da8060
ahoj18 <ahoj18.roblox@gmail.com>
2022-05-29 15:17:55 +0200

    3 files modified

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'SCP Nadace',
+  tagline: 'Dokumentace',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -90,7 +90,7 @@ const config = {
             ],
           },
           {
-            title: 'Community',
+            title: 'Odkazy:',
             items: [
               {
                 label: 'Stack Overflow',
